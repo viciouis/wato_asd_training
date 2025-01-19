f517865ab11f72e1c1f3d31aca13f675e598f0bd
viciouis <aryaen.sharma21@gmail.com>
2025-01-19 17:02:54 -0500

    testing

--- src/robot/costmap/include/costmap_node.hpp
+++ src/robot/costmap/include/costmap_node.hpp
@@ -1,16 +1,28 @@
 #ifndef COSTMAP_NODE_HPP_
 #define COSTMAP_NODE_HPP_
-
+ 
 #include "rclcpp/rclcpp.hpp"
-
+#include "std_msgs/msg/string.hpp"
+#include "sensor_msgs/msg/LaserScan.hpp"
 #include "costmap_core.hpp"
+
 
 class CostmapNode : public rclcpp::Node {
   public:
     CostmapNode();
-
+    
+    // Place callback function here
+    void publishMessage();
+    void subscribeMessage(const sensor_msgs::msg::LaserScan msg);
+    
   private:
     robot::CostmapCore costmap_;
-};
+    // Place these constructs here
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr string_pub_;
+    rclcpp::TimerBase::SharedPtr timer_;
 
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscription_;
+    sensor_msgs::msg::LaserScan OccupancyGrid[][];
+};
+  
 #endif 
